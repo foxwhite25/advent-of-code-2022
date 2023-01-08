8fa6a7d2565d4d5a478707081c1d64ba7105a19b
Fox_white <vct.xie@gmail.com>
2023-01-08 20:09:36 +0800

    part 3

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 # 🎄 Advent of Code 2022
 
 Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.rust-lang.org/).
+
+## 🎅🏻 Usage
+
+Run all solutions:
+
+```bash
+cargo run all
+```
+
+Run a specific day:
+
+```bash
+cargo run solve <day>
+```
---
+++ src/bin/03.rs
@@ -0,0 +1,60 @@
+type Input<'a> = Vec<&'a str>;
+
+fn parse(input: &str) -> Input {
+    input.lines().collect()
+}
+
+fn priority_func(input: u8) -> u8 {
+    input % 32 + (26 * (input <= 90) as u8)
+}
+
+pub fn part_one(input: Input) -> Option<u32> {
+    Some(
+        input
+            .iter()
+            .filter_map(|&s| {
+                let (a, b) = s.split_at(s.len() / 2);
+                a.as_bytes()
+                    .iter()
+                    .find(|byte| b.as_bytes().contains(byte))
+                    .map(|&byte| priority_func(byte) as u32)
+            })
+            .sum()
+    )
+}
+
+pub fn part_two(input: Input) -> Option<u32> {
+    Some(
+        input
+            .chunks(3)
+            .filter_map(|chunk| {
+                let mut k = chunk.iter();
+                let a = k.next()?.as_bytes();
+                let b = k.next()?.as_bytes();
+                let c = k.next()?.as_bytes();
+
+                a.iter()
+                    .find(|byte| b.contains(byte) && c.contains(byte))
+                    .map(|&byte| priority_func(byte) as u32)
+            }).sum()
+    )
+}
+
+advent_of_code::main!(3);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(parse(&advent_of_code::template::read_file("examples", 3)));
+        assert_eq!(result, Some(157));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(parse(&advent_of_code::template::read_file("examples", 3)));
+        assert_eq!(result, Some(70));
+    }
+}
