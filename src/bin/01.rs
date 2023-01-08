28e5da959c8e88055d9f1bb060b9a44c817f04c8
Fox_white <vct.xie@gmail.com>
2023-01-08 15:41:55 +0800

    refactor the tester

---
+++ src/bin/01.rs
@@ -0,0 +1,39 @@
+use itertools::{sorted};
+
+type Input = Vec<u32>;
+
+fn parse(input: &str) -> Input {
+    input
+        .split("\n\n")
+        .map(|c| c.lines().filter_map(|l| l.parse::<u32>().ok()).sum())
+        .collect()
+}
+
+pub fn part_one(input: Input) -> Option<u32> {
+    input.iter().max().copied()
+}
+
+pub fn part_two(input: Input) -> Option<u32> {
+    Some(
+        sorted(input).rev().take(3).sum()
+    )
+}
+
+advent_of_code::main!(1);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(parse(&advent_of_code::template::read_file("examples", 1)));
+        assert_eq!(result, Some(72511));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(parse(&advent_of_code::template::read_file("examples", 1)));
+        assert_eq!(result, Some(45000));
+    }
+}
